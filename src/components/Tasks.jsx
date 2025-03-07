cadbb49f46226350d417f7c95e9f3578d780ae6d
Gustavo Losch <gustavolosch@icloud.com>
2025-03-07 01:42:55 -0300

    up: task done on click

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, Delete, DeleteIcon, TrashIcon } from "lucide-react";
 
 function Tasks(props) {
   console.log(props);
@@ -6,7 +6,12 @@ function Tasks(props) {
     <ul className="space-y-3 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-white p-2 rounded-md w-full">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md w-full ${
+              task.isCompleted && "line-through"
+            }`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
